e7d5a2368e1b2c73e732ad9c26b3e6c49990a4da
igorfmontenegro <igor.muntenegro@icloud.com>
2023-04-12 17:05:26 -0300

    feat: first update

---
+++ peoples.js
@@ -0,0 +1,136 @@
+let speakers = 
+[
+    {
+        name: "Josivaldo Melo",
+        description: "Proprietário e Professor do Clube do Fera, Docente em várias matérias do CESMAC, Matemático e Mestre em Matemática pela UFAL, Concluinte em Direito pela Ufal e Aprovado em 15 concursos públicos, sendo 8 deles em primeiro lugar.",
+        image: "josivaldo"
+    },
+    {
+        name: "Anne Dayse",
+        description: "Docente em Engenharia Civil, Sistemas de Informação e Farmácia no CESMAC e na Pitágoras. Graduada em Química pela UFRN e em Farmácia pela UFAL, tem Mestrado em Fármacos e Produtos Naturais pela UFPB e Doutorado em Química e Biotecnologia pela UFAL.",
+        image: "annedayse"
+    },
+    {
+        name: "João Vicente",
+        description: "Fundador e gestor da ARTMÓDULO, Engenheiro Civil pela UERJ, tem 30 anos de experiência com drywall e mais de 60 mil metros quadrados montados.",
+        image: "joaovincente"
+    },
+    {
+        name: "Emmanuel Junior",
+        description: "CEO da SOARES NOBRE Imóveis, Corretor de imóveis, Engenheiro Civil pelo CESMAC com MBA em Marketing pela FGV.",
+        image: "emmanuel"
+    },
+    {
+        name: "Adriano Moura",
+        description: "Sócio e Fundador do Adriano Moura Arquitetos, Arquiteto e Urbanista pela FAUPE, fez projetos em São Paulo, Minas Gerais, Alagoas e outros estados, com destaque para o Ed. Studio Design, Acqua Suites e New Time.",
+        image: "adriano"
+    },
+    {
+        name: "Cristian Marcus",
+        description: "Diretor técnico da Construtora DELMAN, Engenherio Civil e Membro do Comitê Técnico da ADEMI.",
+        image: "cristian"
+    },
+    {
+        name: "Fernando Régis",
+        description: "Diretor técnico da STANK Construções, Engenheiro Civil e Coordenador da Norma de Inspeção Predial de Alagoas.",
+        image: "fernando"
+    },
+    {
+        name: "Cezar Nantes",
+        description: "Advogado e Especialista em Direito Condominial",
+        image: "cezar"
+    }
+];
+
+let organizers =
+[
+    {
+        name: "Igor Montenegro",
+        description: "Tesoureiro",
+        image: "igor"
+    },
+    {
+        name: "Anabella Marques",
+        description: "Marketing",
+        image: "anabella"
+    },
+    {
+        name: "Nayra Zambelly",
+        description: "Decoração",
+        image: "nayra"
+    },
+    {
+        name: "Matheus Cunha",
+        description: "Designer",
+        image: "matheuscunha"
+    },
+    {
+        name: "Mateus Batista",
+        description: "Colaborador",
+        image: "mateus"
+    },
+    {
+        name: "Felipe Wanderley",
+        description: "Colaborador",
+        image: "felipe"
+    },
+    {
+        name: "Ferdinando Braga",
+        description: "Colaborador",
+        image: "ferdinando"
+    },
+    {
+        name: "Ivison",
+        description: "Colaborador",
+        image: "ivison"
+    },
+    {
+        name: "Jhonata dos Santos",
+        description: "Colaborador",
+        image: "jhonata"
+    },
+    {
+        name: "Marcelo Menezes",
+        description: "Colaborador",
+        image: "marcelo"
+    },
+    {
+        name: "Raissa Andreza",
+        description: "Colaborador",
+        image: "raissa"
+    },
+    {
+        name: "Vinícius Araujo",
+        description: "Colaborador",
+        image: "vinicius"
+    },
+    {
+        name: "Lucas Albuquerque",
+        description: "Colaborador",
+        image: "lucas"
+    }
+
+]
+
+let sponsors = [
+    {
+        image: "cesmac"
+    },
+    {
+        image: "ademi"
+    }
+    ,
+    {
+        image: "sinduscon"
+    },
+    {
+        image: "placic"
+    },
+    {
+        image: "sanco"
+    },
+    ,
+    {
+        image: "mibasa"
+    }
+]
---
+++ script.js
@@ -0,0 +1,153 @@
+let bannerSlides = ["./images/sicc33-banner.png", "./images/banner-1.png"];
+let cardWrapper = document.querySelector(".card-wrapper");
+let boxProgramming = document.querySelector(".c-day__box");
+let roadMap = document.querySelector(".c-day__roadmap");
+let cProgramming = document.querySelector("#c-programming");
+let subtitleDay = document.querySelector(".c-day__subtitle");
+let displayNone = true;
+
+let photosWrapper = document.querySelector(".photos-wrapper");
+let sponsorWrapper = document.querySelector(".c-sponsors__wrapper");
+
+
+function slide1(){
+    document.getElementById("c-introduction__banner").src="./images/sicc33-banner.png";
+    setTimeout("slide2()", 3000)
+}
+
+function slide2(){
+    document.getElementById("c-introduction__banner").src="./images/banner-1.png"
+    setTimeout("slide1()", 3000);
+}
+
+function createCard(image, name, description){
+    let newCard = document.createElement("div");
+    newCard.classList.add("card");
+    cardWrapper.appendChild(newCard);
+
+    let newImageContent = document.createElement("div");
+    newImageContent.classList.add("image-content");
+    newCard.appendChild(newImageContent);
+
+    let newOverlay = document.createElement("span");
+    newOverlay.classList.add("overlay");
+    newImageContent.appendChild(newOverlay);
+
+    let newCardImage = document.createElement("div");
+    newCardImage.classList.add("card-image");
+    newImageContent.appendChild(newCardImage);
+
+    let newImg = document.createElement("img");
+    newImg.setAttribute("class", "card-img");
+    newImg.setAttribute("src", "./images/speakers/" + image + ".png");
+    newCardImage.appendChild(newImg);
+ 
+    let newCardContent = document.createElement("div");
+    newCardContent.classList.add("card-content");
+    newCard.appendChild(newCardContent);
+
+    let newName = document.createElement("h2");
+    newName.classList.add("name");
+    newCardContent.appendChild(newName)
+    newName.innerText = name;
+
+    let newDescription = document.createElement("div");
+    newDescription.classList.add("description");
+    newCardContent.appendChild(newDescription);
+    newDescription.innerText = description;
+    
+    let newButton = document.createElement("button");
+    newButton.classList.add("button-viewmore");
+    newCardContent.appendChild(newButton);
+    newButton.innerHTML = "Ver mais";
+    
+}
+
+speakers.forEach( speaker => {
+    createCard(speaker.image, speaker.name, speaker.description)
+})
+
+let cardsCarousel = document.querySelectorAll(".card");
+let index = 0;
+let widthWindow = window.innerWidth;
+
+function showMore(increase){
+
+    widthWindow > 1350 ? increase += 3 : increase = 1;
+    widthWindow > 690 ? increase += 1 : increase = 1;
+    
+
+    index = index + increase;
+    index = Math.min(Math.max(index,0), cardsCarousel.length-1);
+    cardsCarousel[index].scrollIntoView({behavior: 'smooth', block:"nearest"});
+ 
+}
+
+function showLess(increase){
+
+    widthWindow > 1350 ? increase -= 3 : increase = -1;
+    widthWindow > 690 ? increase -= 1 : increase = -1;
+
+    index = index + increase;
+    index = Math.min(Math.max(index,0), cardsCarousel.length-1);
+    cardsCarousel[index].scrollIntoView({behavior: 'smooth', block:"nearest"});
+ 
+}
+
+boxProgramming.addEventListener("click", showRoadMap);
+
+function showRoadMap(){
+
+    if (displayNone === true){
+        roadMap.style.opacity = "1";
+        roadMap.style.height = "auto";
+        displayNone = false;
+        cProgramming.style.height = "auto";
+        subtitleDay.innerHTML = "- DIA 26 de NOVEMBRO";
+    } else{
+        roadMap.style.height = "0";
+        roadMap.style.opacity = "0";
+        displayNone = true;
+        cProgramming.style.height = "auto";
+        subtitleDay.innerHTML = "+ DIA 26 de NOVEMBRO";
+    }
+}
+
+
+function newOrganization(img, name, cargo){
+    let organizationSingle = document.createElement("div");
+    organizationSingle.classList.add("organization-single");
+    photosWrapper.appendChild(organizationSingle);
+
+    let imgSingle = document.createElement("img");
+    imgSingle.setAttribute("class", "img-single");
+    imgSingle.setAttribute("src", "./images/organizers/" + img + ".png")
+    organizationSingle.appendChild(imgSingle);
+
+    let descriptionSingle = document.createElement("p");
+    organizationSingle.appendChild(descriptionSingle);
+    descriptionSingle.innerHTML = "<strong>" + name + "</strong>" + "<br>" + cargo;
+    descriptionSingle.setAttribute("style", "text-align: center");
+
+    
+}
+
+
+organizers.forEach( organizer => {
+    newOrganization(organizer.image, organizer.name, organizer.description);
+});
+
+function newSponsor(img){
+    let sponsorSingle = document.createElement("div");
+    sponsorSingle.classList.add("c-sponsor__single");
+    sponsorWrapper.appendChild(sponsorSingle);
+
+    let sponsorImg = document.createElement("img");
+    sponsorImg.setAttribute("class", "c-sponsor__img");
+    sponsorImg.setAttribute("src", "./images/sponsors/" + img + ".png");
+    sponsorSingle.appendChild(sponsorImg)
+}
+
+sponsors.forEach( sponsor => {
+    newSponsor(sponsor.image)
+});
